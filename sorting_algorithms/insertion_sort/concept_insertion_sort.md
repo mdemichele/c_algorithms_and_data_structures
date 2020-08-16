b70629815cbcd40f75e7cb689cb986c7272a6706
Matthew DeMichele <Matt@Matthews-MacBook-Air.local>
2020-08-16 12:07:35 -1000

    insertion sort

--- sorting_algorithms/insertion_sort/concept_insertion_sort.md
+++ sorting_algorithms/insertion_sort/concept_insertion_sort.md
@@ -1,13 +1,21 @@
+## Concept of Insertion Sort 
+
+
 ## Steps of Insertion Sort
 
 Given a list of unsorted numbers:
 
-1. Set a marker for the sorted section after the first number in the list
-2. Repeat steps 3 through 5 until the unsorted section is empty
-3.   Select the first unsorted number
-4.   Swap this number to the left until it arrives at the correct sorted position
-5.   Advance the marker to the right one position
-6. Stop
+1. Set the key to the second item in the list.  
+3. Loop from arr[key] to arr[0]. Compare each item in list to the key value. If the item is greater than key, move it one to the left. If the item is smaller than the key, exit loop.
+4. Once loop is exited, swap the current index value for the key value (This should place key value in correct order position).
+5. Advance the key to the next item in list. Repeat steps 3-4. 
+6. Once the key reaches the end of the list, you are done. 
+
+## Time Complexity
+The time complexity of insertion sort is O(n^2). 
+
+## Advantages and Disadvantages 
 
 ## Resources
 https://courses.cs.vt.edu/csonline/Algorithms/Lessons/InsertionSort/index.html
+https://en.wikipedia.org/wiki/Insertion_sort
--- sorting_algorithms/insertion_sort/insertion_sort.c
+++ sorting_algorithms/insertion_sort/insertion_sort.c
@@ -2,67 +2,35 @@
 #include <stdio.h>
 
 
-void	insertion_sort(int *arr)
-{
-	int marker;
-	int i;
-	int temp;
-
-	marker = 1;
-
-	while (arr[marker])
-	{
-		i = marker;
-		while (arr[i] < arr[i - 1] && i > 0)
-		{	
-			temp = arr[i];
-			arr[i] = arr[i - 1];
-			arr[i - 1] = temp;
-			i--;
+void	insertion_sort(int arr[], int n) {
+	for (int i = 1; i < n; i++) {
+		int keyValue = arr[i];
+		int j = i;
+		while (j > 0 && arr[j - 1] > keyValue) {
+			arr[j] = arr[j - 1];
+			j--;
 		}
-		marker++;
+		arr[j] = keyValue;
 	}
 }
 
-//Test Main
+void print_arr(int arr[], int n) {
+	for (int i = 0; i < n; i++) {
+		printf("%d", arr[i]);
+	}
+	printf("\n");
+}
 
-int	main()
-{
-	int *ARRAY;
-	int continue_filling;
-	int ARRAY_LENGTH, is_sorted, TEMP;
+int	main() {
 	
-	ARRAY = NULL;
-	continue_filling = 1;
-	ARRAY_LENGTH = 0;
-
-	while (continue_filling)
-	{
-		printf("Insert a value into the array (current size: %d)\n", ARRAY_LENGTH + 1);
-		ARRAY = (int *)realloc(ARRAY, sizeof(int) * (ARRAY_LENGTH));
-		scanf("%d", &ARRAY[ARRAY_LENGTH]);
-		ARRAY_LENGTH += 1;
-		printf("Do you want to add another value (1: Yes/ 0: No, sort it!)?\n");
-		scanf("%d", &continue_filling);
-	}
-
-	for (int i = 0; i < ARRAY_LENGTH; i++)
-	{
-		printf("%d, ", ARRAY[i]);
-	}
-
-	printf("\n");
-	printf("sorted: \n");
-	insertion_sort(ARRAY);
-
-	for (int i = 0; i < ARRAY_LENGTH; i++)
-	{
-		printf("%d, ", ARRAY[i]);
-	}
-
-	printf("\n");
-
-	return (0);
+	int arr[] = { 1, 2, 5, 8, 3 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	
+	print_arr(arr, n);
+	insertion_sort(arr, n);
+	print_arr(arr, n);
+	
+	return 0;
 }
 
 
