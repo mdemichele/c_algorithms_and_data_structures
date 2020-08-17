0de88d08031fb8c9c97ada0b5327d5d58bb032a2
Matthew DeMichele <Matt@Matthews-MacBook-Air.local>
2020-08-16 14:40:05 -1000

    merge sort sorta done

--- sorting_algorithms/merge_sort/merge_sort.c
+++ sorting_algorithms/merge_sort/merge_sort.c
@@ -1,7 +1,50 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void merge(int arr[], int start, int mid, int end) {
+  // temporary array
+  int temp[end - start + 1];
+  
+  // iterators
+  int i = start, j = mid+1, k = 0;
+  
+  // traverse both arrays and in each iteration add smaller of both elements in temp 
+  while (i <= mid && j <= end) {
+    if (arr[i] <= arr[j]) {
+      temp[k] = arr[i];
+      i++; k++;
+    } else {
+      temp[k] = arr[j];
+      j++; k++;
+    }
+  }
+  
+  // add elements left in the first interval
+  while (i <= mid) {
+    temp[k] = arr[i];
+    i++; k++;
+  }
+  
+  // add elements left in the second interval
+  while (j <= end) {
+    temp[k] = arr[j];
+    j++; k++;
+  }
+  
+  // Copy temp to original interval
+  for (i = start; i <= end; i++) {
+    arr[i] = temp[i - start];
+  }
+}
 
+void merge_sort(int arr[], int start, int end) {
+  if (start < end) {
+    int mid = (start + end) / 2;
+    merge_sort(arr, start, mid);
+    merge_sort(arr, mid+1, end);
+    merge(arr, start, mid, end);
+  }
+}
 
 
 
@@ -18,6 +61,8 @@ int main() {
   int arr[] = { 9, 8, 4, 6, 1};
   int n = sizeof(arr) / sizeof(arr[0]);
   
+  print_arr(arr, n);
+  merge_sort(arr, 0, n - 1);
   print_arr(arr, n);
   
   return 0;
---
+++ sorting_algorithms/merge_sort/merge_sort.md
@@ -0,0 +1,13 @@
+## Merge Sort Concepts
+Merge Sort is a divide-and-conquer algorithm that utilizes recursion. Essentially, the algorithm recursively splits the unsorted list into sublists until the sublists are each one element long. Then, it merges those sublists together in order, forming larger and larger sublists until there is only one list again. At that point, the list should be sorted and back to the correct length.
+
+## Merge Sort Steps
+1. Divide the unsorted list into n sublists (each sublist should contain only one element, at which point it is considered to be sorted).
+2. Repeatedly merge the sublists to produce larger sorted sublists until there is only one sublist remaining. The remaining list will be sorted. 
+
+
+## Time Complexity
+The running time of Merge sort is O(n log n). I will explain this in greater detail soon.
+## Resources
+["Merge Sort" - Wikipedia](https://en.wikipedia.org/wiki/Merge_sort)
+
