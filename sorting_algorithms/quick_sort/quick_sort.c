3cab22ee2936ec60dce43cab494a335ba6c68afb
Matthew DeMichele <Matt@Matthews-MacBook-Air.local>
2021-08-03 16:27:49 -1000

    added quick sort documentation.

--- sorting_algorithms/quick_sort/quick_sort.c
+++ sorting_algorithms/quick_sort/quick_sort.c
@@ -13,7 +13,7 @@ int partition(int arr[], int start, int end) {
   // index of smaller element
   int i = start - 1; 
   
-  for(int j = start; j <= end - 1; j++) {
+  for(int j = start; j < end; j++) {
     // If current element is smaller than or equal to pivot
     if (arr[j] <= pivot) {
       i++;
@@ -34,16 +34,21 @@ void quick_sort(int arr[], int start, int end) {
 
 void print_arr(int arr[], int n) {
   for (int i = 0; i < n; i++) {
-    printf("%d", arr[i]);
+    printf("%d ", arr[i]);
   }
   printf("\n");
 }
 
 int main() {
+  printf("-----------TEST 1--------------\n");
+  printf("Original Array: ");
   int arr[] = { 5, 9, 2, 6, 3 };
   int n = sizeof(arr) / sizeof(arr[0]);
-  
   print_arr(arr, n);
+  
+  quick_sort(arr, 0, n - 1);
+  
+  printf("Sorted Array: ");
   quick_sort(arr, 0, n - 1);
   print_arr(arr, n);
   
--- sorting_algorithms/quick_sort/quick_sort.md
+++ sorting_algorithms/quick_sort/quick_sort.md
@@ -1,6 +1,12 @@
 ## Quick Sort Concepts
 Quick Sort is a divide-and-conquer algorithm. 
 
+# Steps 
+1. Choose an element to be the pivot 
+2. Partition the array based on where the pivot is located. You'll have a left partition that is less than the pivot and a right partition that is greater than the pivot. 
+3. Recursively apply quick sort on left partition. 
+4. Recursively apply quick sort to the right partition.
+
 ## Time Complexity
 Quick sort can have a worst case time complexity of O(n^2). But if the pivot is chosen correctly, it can have an average time complexity of O(n log n).
 
